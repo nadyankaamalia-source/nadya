d61b6541d501236ec4b8ab36e7e6af45fe88960b
nadyankaamalia-source <nadyankaamalia@gmail.com>
2026-05-22 14:17:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.title("🎈 Welcome!")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-st.title("This is a title")
+st.title("Good Afternoon")
 st.title("_Happy_ Birthday :Manahel[cool] :sunglasses:")
 
 
