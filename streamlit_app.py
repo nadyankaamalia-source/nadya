4f96d4a1326529c8c5cc1cb7495333b2f7c848b7
nadyankaamalia-source <nadyankaamalia@gmail.com>
2026-05-22 14:20:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 st.title("Good Afternoon")
-st.title("_Happy_ Birthday :Manahel[shit] : shit")
+st.title("_Streamlit_ is :blue[cool] :kissing:")
 
 
