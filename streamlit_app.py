84f5f68bef1d0cda6cdd73b3083186c68201a6a2
nadyankaamalia-source <nadyankaamalia@gmail.com>
2026-05-23 10:49:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,15 @@ st_tittle("Dashboard")
 st.header("Laporan Bulanan")
 st.subheader("Monthly Expenses")
 st.caption ("Made with love")
+
+import streamlit as st
+
+st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
+
+with st.expander("See explanation"):
+    st.write('''
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    ''')
+    st.image("https://static.streamlit.io/examples/dice.jpg")
