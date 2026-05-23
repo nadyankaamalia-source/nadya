302f1af001051fbb883f9ca31002163c3f4df58f
nadyankaamalia-source <nadyankaamalia@gmail.com>
2026-05-23 10:15:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,13 @@
-import streamlit as st
-
+import streamlit as     
 st.set_page_config(
-    page_title="Finance Dashboard",
+    page_title="Kuliah Praktisi",
+    pagee_icon="tada",
     layout="wide"
+)
+
+
+#Hirarki teks
+st_tittle("Dashboard")
+st.header("Laporan Bulanan")
+st.subheader("Monthly Expenses")
+st.caption ("Made with love")
